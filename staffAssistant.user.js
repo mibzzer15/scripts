ae8916024f84d19eb06268fcc3c4e2f425a37332
tylernelson224 <82922290+tylernelson224@users.noreply.github.com>
2022-11-15 19:34:50 -0600

    Update staffAssistant.user.js

--- staffAssistant.user.js
+++ staffAssistant.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Staff Assistant
-// @version      1.4.6
+// @version      1.4.7
 // @description  Select advertising phases and personnel target in the building overview
 // @author       DrTraxx and translated by Mibzzer15
 // @namespace https://github.com/mibzzer15
