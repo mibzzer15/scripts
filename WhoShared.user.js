4d58fce7fd0744dd7c4eb148b0a21b5795a90615
tylernelson224 <82922290+tylernelson224@users.noreply.github.com>
2022-11-15 18:25:43 -0600

    Create WhoShared.user.js

---
+++ WhoShared.user.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         WhoShared
+// @version      1.0.1
+// @description  Shows who shared the mission. Search function then also searches for names.
+// @author       HerrWaldgott
+// @include      *://www.missionchief.com/
+// @grant        none
+// @namespace    https://github.com/mibzzer15
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    $('#mission_list_alliance > .missionSideBarEntry, #mission_list_alliance_event > .missionSideBarEntry').each(async function(){
+        var $this = $(this);
+        var $attributes = $this[0].attributes;
+        var missionId = +($attributes).mission_id.value;
+        await new Promise(resolve => {
+            $.get("https://www.missionchief.com/missions/" + missionId, function (data, status) {
+                var parser = new DOMParser();
+                var htmlDoc = parser.parseFromString(data, 'text/html');
+                var div = htmlDoc.getElementById('col_left');
+                var nameDiv = div.getElementsByClassName('alert-info');
+                var name = nameDiv[0].childNodes[1].innerHTML;
+
+                var id = "mission_address_" + missionId;
+                $('#' + id).text($('#' + id).text() + " - " + name);
+                $('#mission_' + missionId).attr('search_attribute', $('#mission_' + missionId).attr('search_attribute') + ", " + name);
+                window.setTimeout(resolve, 100);
+            });
+        });
+    });
+})();
